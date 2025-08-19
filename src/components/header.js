d587cb05e9fbe723a1100269cc5a4e868d6ba99f
wizpzy <wizpzy@gmail.com>
2025-08-19 20:20:18 +0700

    fix: handle session not found

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ export default async function Header() {
     const session = await getAuthSession();
     const isLogin = "userId" in session;
     const name = isLogin ? (await getUserById(session.userId, {f_name: true, l_name: true})) : null;
-    const role = session.role;
+    const role = session?.role || null;
 
     return (
         <HeaderClient isLogin={isLogin} fName={name?.f_name} lName={name?.l_name} role={role} />
