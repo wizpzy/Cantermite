6baea6da5d9f38b3f64b94e45b7ab4e71796d842
wizpzy <wizpzy@gmail.com>
2025-09-20 21:40:40 +0700

    refactor: parallel fetching

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,23 @@ import GenreList from "@/components/genreList";
 import Section from "@/components/section";
 import styles from "./page.module.css";
 
+const getBooks = async () => {
+  const response = await axios.get("http://localhost:3000/api/books?sort=popular");
+  return response.data.slice(0, 5);
+}
+
+const getGenres = async () => {
+  const response = await axios.get("http://localhost:3000/api/genres");
+  return response.data.slice(0, 5);
+}
+
 export default async function Home() {
-  const bookResponse = await axios.get("http://localhost:3000/api/books?sort=popular");
-  const bookData = bookResponse.data.slice(0, 5);
-  const genreResponse = await axios.get("http://localhost:3000/api/genres");
-  const genreData = genreResponse.data.slice(0, 5);
+  const [books, genres] = await Promise.all([getBooks(), getGenres()]);
 
   return (
     <div className={styles.page}>
-      <Section title="หนังสือแนะนำ" seeAllHref="/books" children={<BookList books={bookData} />} />
-      <Section title="หมวดหมู่" seeAllHref="/genres" children={<GenreList genres={genreData} />} />
+      <Section title="หนังสือแนะนำ" seeAllHref="/books" children={<BookList books={books} />} />
+      <Section title="หมวดหมู่" seeAllHref="/genres" children={<GenreList genres={genres} />} />
     </div>
   );
 }
