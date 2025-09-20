ee015ce91905ed3846aab996aca1388233419261
wizpzy <wizpzy@gmail.com>
2025-09-20 21:44:21 +0700

    fix: genreCard use button component

--- src/components/genreCard.js
+++ src/components/genreCard.js
@@ -1,10 +1,12 @@
+import Button from "./button";
 import styles from "./genreCard.module.css";
 import Image from "next/image";
 
 export default function GenreCard({ genre_name }) {
     return (
-        <div className={styles.card}>
-            {genre_name}
-        </div>
+        <Button children={genre_name} className={styles.card} />
+        // <div className={styles.card}>
+        //     {genre_name}
+        // </div>
     );
 }
