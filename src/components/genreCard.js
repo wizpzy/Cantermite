bad630c75505a28a9d0de8f056b906d25079e3ee
wizpzy <wizpzy@gmail.com>
2025-09-23 02:00:18 +0700

    fix: fixing interactive inside interactive

--- src/components/genreCard.js
+++ src/components/genreCard.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import Button from "./button";
 import styles from "./genreCard.module.css";
 import Image from "next/image";
 
 export default function GenreCard({ genre_name }) {
     return (
-        <Button className={styles.card}>{genre_name}</Button>
+        <Link href={`/genres/${genre_name}`} className={styles.linkButton}>{genre_name}</Link>
+        // <Button className={styles.card}>{genre_name}</Button>
     );
 }
