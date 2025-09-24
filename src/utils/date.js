44b93c0faf7e329ea95f1d25950de11517cade96
wizpzy <wizpzy@gmail.com>
2025-09-25 00:01:52 +0700

    feat: update date util

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,4 +1,4 @@
-export const getToday = () => {
+export function getToday() {
   const today = new Date();
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, "0");
@@ -6,8 +6,19 @@ export const getToday = () => {
   return `${year}-${month}-${day}`;
 }
 
-export const getDueDate = (borrowDate) => {
+export function getDueDate(borrowDate) {
     const dueDate = new Date(borrowDate);
     dueDate.setDate(dueDate.getDate() + 14);
-    return dueDate.toISOString().split("T")[0];
+    const year = dueDate.getFullYear();
+    const month = String(dueDate.getMonth() + 1).padStart(2, "0");
+    const day = String(dueDate.getDate()).padStart(2, "0");
+    return `${month}/${day}/${year}`;
+}
+
+export function reformatDueDate (dateString) {
+    const date = dateString.split("/");
+    const year = date[2];
+    const month = String(date[0]).padStart(2, "0");
+    const day = String(date[1]).padStart(2, "0");
+    return `${year}-${month}-${day}`;
 }
