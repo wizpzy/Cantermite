a027b204e8223ac6a30d29f1f1513878cdae0872
wizpzy <wizpzy@gmail.com>
2025-11-03 06:13:56 +0700

    fix: search bar styling

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -2,8 +2,8 @@
 
 export default function SearchBar() {
     return (
-        <div className="w-[45vw] h-[45px]">
-            <input className="w-full h-full text-sm bg-(--white) text-(--black) px-5 py-0 rounded-[20px]" type="text" placeholder="ค้นหา . . ." />
+        <div className="w-[45vw] h-[45px] flex-1">
+            <input className="w-full h-full text-sm bg-(--white) text-(--black) px-5 py-0 rounded-[20px] border-(--lightgrey1) border-2" type="text" placeholder="ค้นหา . . ." />
         </div>
     );
 }
