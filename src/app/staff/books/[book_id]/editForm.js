7a436a90c180e71b7418b8dbece2d97d7a5f0631
wizpzy <wizpzy@gmail.com>
2025-11-09 04:15:16 +0700

    refactor: editForm

--- src/app/staff/books/[book_id]/editForm.js
+++ src/app/staff/books/[book_id]/editForm.js
@@ -3,103 +3,102 @@
 import { editBook } from "@/lib/actions/books/editBook";
 import { Check, ChevronDown } from "lucide-react";
 import { useActionState } from "react";
+import ImageDropZone from "@/components/imageDropZone";
 import SuccessModal from "@/components/successModal";
+import { getBookCover } from "@/utils/imagePath";
 
 export default function EditForm({ bookData, genreChoices }) {
-    const [state, formAction, isPending] = useActionState(editBook, {success: false});
+    const [state, formAction, isPending] = useActionState(editBook, { success: false });
+    const imageUrl = bookData.image_path ? getBookCover(bookData.image_path, 'L') : '/noImage.png';
+    console.log(imageUrl)
 
     return (
         <>
-            <form
-                action={formAction}
-                className="flex h-fit w-6/10 flex-col gap-4 rounded-[20px] border-2 border-(--lightgrey1) p-10"
-            >
-                <h2 className="text-base font-medium">รายละเอียดหนังสือ</h2>
-                <div className="flex flex-col gap-5">
-                    <div className="flex">
-                        <div className="flex w-1/2 flex-col gap-2 text-sm">
-                            <label htmlFor="title" className="font-medium">ชื่อหนังสือ</label>
-                            <input
-                                name="title"
-                                id="title"
-                                className="w-7/10 rounded-[20px] border-2 border-(--lightgrey1) px-3 py-2 text-(--darkblue)"
-                                placeholder={bookData.title ?? "-"}
-                            />
+            <div className="flex flex-col h-full w-full gap-10 rounded-[25px] bg-[white] p-10 shadow-[0_0_45.4px_rgba(0,0,0,0.2)]">
+                <h2 className="font-medium">แก้ไขหนังสือ</h2>
+                <form className="flex flex-col gap-10" action={formAction}>
+                    <div className="flex gap-10">
+                        <div className="flex flex-col gap-4 w-7/20">
+                            <span className="font-medium text-sm">รูปปกหนังสือ</span>
+                            <ImageDropZone name="imageFile" defaultImage={{ dataURL: imageUrl, fileInfo: { name: `ภาพปก ${bookData.title}`, size: 0 } }} />
                         </div>
-                        <div className="flex w-1/2 flex-col gap-2 text-sm">
-                            <label htmlFor="publisher" className="font-medium">ชื่อสำนักพิมพ์</label>
-                            <input
-                                name="publisher"
-                                id="publisher"
-                                className="w-7/10 rounded-[20px] border-2 border-(--lightgrey1) px-3 py-2 text-(--darkblue)"
-                                placeholder={bookData.publisher ?? "-"}
-                            />
-                        </div>
-                    </div>
-                    <div className="flex">
-                        <div className="flex w-1/2 flex-col gap-2 text-sm">
-                            <label htmlFor="author" className="font-medium">ชื่อผู้เขียน</label>
-                            <input
-                                name="author"
-                                id="author"
-                                className="w-7/10 rounded-[20px] border-2 border-(--lightgrey1) px-3 py-2 text-(--darkblue)"
-                                placeholder={bookData.author ?? "-"}
-                            />
-                        </div>
-                        <div className="flex w-1/2 flex-col gap-2 text-sm">
-                            <label htmlFor="year" className="font-medium">
-                                ปีที่พิมพ์
-                                <span className="font-normal text-red-400"> {state.yearError ? `*${state.yearError}*` : ""}</span>
-                            </label>
-                            <input
-                                type="number"
-                                name="year"
-                                id="year"
-                                className="w-7/10 rounded-[20px] border-2 border-(--lightgrey1) px-3 py-2 text-(--darkblue)"
-                                placeholder={bookData.year ?? "-"}
-                            />
-                        </div>
-                    </div>
-                    <div className="flex">
-                        <div className="flex w-1/2 flex-col gap-2 text-sm">
-                            <label htmlFor="genre" className="font-medium">หมวดหมู่</label>
-                            <div className="relative w-7/10">
-                                <select
-                                    name="genre"
-                                    id="genre"
-                                    className="w-full rounded-[20px] border-2 border-(--lightgrey1) px-3 py-2 text-(--darkblue) appearance-none"
-                                    defaultValue={bookData.genre_name}
-                                >
-                                    {genreChoices.map((genre) => (
-                                        <option key={genre.genre_id} value={genre.genre_name} className="rounded-[20px]">
-                                            {genre.genre_name}
-                                        </option>
-                                    ))}
-                                </select>
-                                <ChevronDown color="var(--darkblue)" strokeWidth={2} className="absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none" />
+                        <div className="flex flex-col gap-8 w-13/20">
+                            <div className="flex flex-col gap-4 w-7/10">
+                                <label htmlFor="title" className="font-medium text-sm">ชื่อหนังสือ <span className="text-red-400">* {state.error?.titleError ? state.error.titleError + " *" : ""}</span> </label>
+                                <input
+                                    name="title"
+                                    id="title"
+                                    className={`rounded-[20px] border-2 border-(--lightgrey1) px-5 py-3 text-(--darkblue) text-sm ${state.error?.titleError ? "bg-red-200" : ""}`}
+                                    placeholder={bookData.title ?? "-"}
+                                />
+                            </div>
+                            <div className="flex flex-col gap-4 w-7/10">
+                                <label htmlFor="author" className="font-medium text-sm">ชื่อผู้เขียน <span className="text-red-400">* {state.error?.authorError ? state.error.authorError + " *" : ""}</span> </label>
+                                <input
+                                    name="author"
+                                    id="author"
+                                    className={`rounded-[20px] border-2 border-(--lightgrey1) px-5 py-3 text-(--darkblue) text-sm ${state.error?.authorError ? "bg-red-200" : ""}`}
+                                    placeholder={bookData.author ?? "-"}
+                                />
+                            </div>
+                            <div className="flex flex-col gap-4 w-7/10">
+                                <label htmlFor="publisher" className="font-medium text-sm">ชื่อสำนักพิมพ์</label>
+                                <input
+                                    name="publisher"
+                                    id="publisher"
+                                    className={`rounded-[20px] border-2 border-(--lightgrey1) px-5 py-3 text-(--darkblue) text-sm`}
+                                    placeholder={bookData.publisher ?? "-"}
+                                />
+                            </div>
+                            <div className="flex gap-7 w-7/10 justify-between">
+                                <div className="flex flex-col gap-4 flex-1">
+                                    <label htmlFor="year" className="font-medium text-sm">ปีที่พิมพ์ <span className="text-red-400">* {state.error?.yearError ? state.error.yearError + " *" : ""}</span> </label>
+                                    <input
+                                        type="number"
+                                        name="year"
+                                        id="year"
+                                        className={`rounded-[20px] border-2 border-(--lightgrey1) px-5 py-3 text-(--darkblue) text-sm ${state.error?.yearError ? "bg-red-200" : ""}`}
+                                        placeholder={bookData.year ?? "-"}
+                                    />
+                                </div>
+                                <div className="flex flex-col gap-4 flex-1">
+                                    <label htmlFor="language" className="font-medium text-sm">ภาษา <span className="text-red-400">* {state.error?.languageError ? state.error.languageError + " *" : ""}</span> </label>
+                                    <input
+                                        name="language"
+                                        id="language"
+                                        className={`rounded-[20px] border-2 border-(--lightgrey1) px-5 py-3 text-(--darkblue) text-sm ${state.error?.languageError ? "bg-red-200" : ""}`}
+                                        placeholder={bookData.language ?? "-"}
+                                    />
+                                </div>
+                                <div className="flex flex-col gap-4 flex-1">
+                                    <label htmlFor="genre" className="font-medium text-sm">หมวดหมู่ <span className="text-red-400">*</span> </label>
+                                    <div className="relative">
+                                        <select
+                                            name="genre"
+                                            id="genre"
+                                            className="w-full rounded-[20px] border-2 border-(--lightgrey1) px-5 py-3 text-(--darkblue) text-sm appearance-none"
+                                            defaultValue={bookData.genre_name}
+                                        >
+                                            {genreChoices.map((genre) => (
+                                                <option key={genre.genre_id} value={genre.genre_name} className="rounded-[20px]">
+                                                    {genre.genre_name}
+                                                </option>
+                                            ))}
+                                        </select>
+                                        <ChevronDown color="var(--darkblue)" strokeWidth={2} className="absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="flex w-1/2 flex-col gap-2 text-sm">
-                            <label htmlFor="language" className="font-medium">ภาษา</label>
-                            <input
-                                name="language"
-                                id="language"
-                                className="w-7/10 rounded-[20px] border-2 border-(--lightgrey1) px-3 py-2 text-(--darkblue)"
-                                placeholder={bookData.language ?? "-"}
-                            />
-                        </div>
                     </div>
-                </div>
-                <input name="bookId" type="hidden" value={bookData.book_id} />
-                <div className="flex justify-center">
-                    <button type="submit" className="button w-fit justify-self-center mt-10">
-                        <Check strokeWidth={3} />
-                        แก้ไขหนังสือ
-                    </button>
-                </div>
-            </form>
+                    <input name="bookId" type="hidden" value={bookData.book_id} />
+                    <div className="flex justify-center">
+                        <button type="submit" className="button bg-(--black) w-40">แก้ไขหนังสือ</button>
+                    </div>
+                </form>
+            </div>
             {state.success && (
-                <SuccessModal text="แก้ไขหนังสือสำเร็จแล้ว" onClose={() => location.reload()} />
+                <SuccessModal text={`แก้ไขหนังสือ ${state.title ?? bookData.title} สำเร็จแล้ว`} onClose={() => location.reload()} />
             )}
         </>
     );
--- src/app/staff/books/[book_id]/page.js
+++ src/app/staff/books/[book_id]/page.js
@@ -1,14 +1,11 @@
 import { getAllGenres, getBookById } from "@/lib/db";
 import BreadCrumb from "@/components/breadCrumb";
-import Image from "next/image";
 import EditForm from "./editForm";
-import { getBookCover } from "@/utils/imagePath";
 
 export default async function EditBookPage({ params }) {
     const { book_id } = await params;
     const book = await getBookById(book_id);
     const genres = await getAllGenres();
-    const imageUrl = book.image_path ? getBookCover(book.image_path, 'L') : '/noImage.png';
     return (
         <div className="m-10">
             <BreadCrumb
@@ -17,15 +14,7 @@ export default async function EditBookPage({ params }) {
                     { label: "ดูรายละเอียดหนังสือ" },
                 ]}
             />
-            <div className="flex flex-col w-full h-full gap-10 rounded-[25px] bg-[white] p-10 shadow-[0_0_45.4px_rgba(0,0,0,0.2)]">
-                <h2 className="font-medium">แก้ไขหนังสือ</h2>
-                <div className="flex gap-10 w-full justify-evenly items-center">
-                    <div className="overflow-hidden w-[350px] h-[560px] rounded-[20px] relative">
-                        <Image src={imageUrl} alt={`${book.title} cover / ${book.image_path}`} fill priority />
-                    </div>
-                    <EditForm bookData={book} genreChoices={genres} />
-                </div>
-            </div>
+            <EditForm bookData={book} genreChoices={genres} />
         </div>
     );
 }
