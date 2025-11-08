915627a73b56049463c2416e41126a9c2ccde87d
wizpzy <wizpzy@gmail.com>
2025-11-09 06:49:31 +0700

    feat: book searching for staff page

---
+++ src/app/staff/books/filterSection.js
@@ -0,0 +1,38 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Table from "./table";
+
+export default function FilterSection({ booksData }) {
+    const [filter, setFilter] = useState("");
+    const [filteredData, setFilteredData] = useState([]);
+
+    useEffect(() => {
+        setFilteredData(booksData.filter((book) => {
+            return book.title.startsWith(filter);
+        }))
+    }, [filter])
+
+    return (
+        <>
+            <div className="flex items-center justify-between gap-5">
+                <h2 className="text-xl font-medium">จัดการหนังสือ</h2>
+                <div className="h-[45px] w-[45vw] flex-1">
+                    <input
+                        className="h-full w-full rounded-[20px] border-2 border-(--lightgrey1) bg-(--white) px-5 py-0 text-sm text-(--black)"
+                        type="text"
+                        placeholder="ค้นหา . . ."
+                        onChange={(e) => setFilter(e.target.value)}
+                    />
+                </div>
+                <Link className="button bg-(--black) px-5" href="/staff/books/create">
+                    <Plus />
+                    เพิ่มหนังสือ
+                </Link>
+            </div>
+            <Table data={filteredData} />
+        </>
+    );
+}
--- src/app/staff/books/page.js
+++ src/app/staff/books/page.js
@@ -1,23 +1,12 @@
-import { Plus } from "lucide-react";
-import SearchBar from "@/components/searchbar";
-import Table from "./table";
 import { getAllBooks } from "@/lib/db";
-import Link from "next/link";
+import FilterSection from "./filterSection";
 
 export default async function ManageBookPage() {
     const booksData = await getAllBooks();
 
     return (
         <div className="m-10 flex h-full flex-col gap-10 rounded-[25px] border-2 border-solid border-(--lightgrey1) p-10">
-            <div className="flex items-center justify-between gap-5">
-                <h2 className="text-xl font-medium">จัดการหนังสือ</h2>
-                <SearchBar />
-                <Link className="button bg-(--black) px-5" href="/staff/books/create">
-                    <Plus />
-                    เพิ่มหนังสือ
-                </Link>
-            </div>
-            <Table data={booksData} />
+            <FilterSection booksData={booksData} />
         </div>
     );
 }
