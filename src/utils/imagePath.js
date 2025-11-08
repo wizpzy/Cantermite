f53779553a9af7946bee8ea73df206d44a707e84
wizpzy <wizpzy@gmail.com>
2025-11-09 04:25:47 +0700

    fix: prevent Next.js caching image

--- src/utils/imagePath.js
+++ src/utils/imagePath.js
@@ -5,7 +5,7 @@ const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.
 export function getBookCover(path, size=null) {
     if (path.endsWith("_cover")) {
         const { data } = supabase.storage.from("book_cover").getPublicUrl(path);
-        return data.publicUrl;
+        return `${data.publicUrl}?t=${Date.now()}`; // Add timestamp to prevent caching
     } else {
         return `https://covers.openlibrary.org/b/id/${path}-${size}.jpg`
     }
